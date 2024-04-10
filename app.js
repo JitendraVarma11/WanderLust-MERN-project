f66d45ac23329379bf4170eef7d53c0682507212
JitendraVarma11 <vjitendra1192002@gmail.com>
2024-04-10 20:18:28 +0530

    changed app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const MongoStore=require("connect-mongo");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -17,10 +17,7 @@ const User = require("./models/user.js");
 const listingRouter = require("./Routes/listings.js");
 const reviewRouter = require("./Routes/review.js");
 const userRouter = require("./Routes/user.js");
-const wrapAsync = require("./utils/wrapAsync.js");
-
-const mongoUrl="mongodb://localhost:27017/wanderlust";
-// const dbUrl=process.env.ATLASDB_URL;
+const mongoUrl = "mongodb://localhost:27017/wanderlust";
 
 async function main() {
   await mongoose.connect(mongoUrl);
@@ -40,25 +37,7 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-
-// const store=MongoStore.create({
-//   mongoUrl:dbUrl,
-//   crypto:{
-//     secret:process.env.SECRET,
-//   },
-//   touchAfter:24*3600,
-// })
-
-// store.on("error",(err)=>{
-//   console.log("ERROR :", err);
-// })
 const sessionOption = {
-  //store,
-store.on("error",(err)=>{
-  console.log("ERROR: ", err);
-})
-const sessionOption = {
-  store,
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
@@ -67,7 +46,7 @@ const sessionOption = {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
   },
-}
+};
 
 app.use(session(sessionOption));
 app.use(flash());
@@ -95,7 +74,6 @@ app.all("*", (req, res, next) => {
 });
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
-  // res.status(statusCode).send(message);
   res.render("error.ejs", { err });
 });
 
